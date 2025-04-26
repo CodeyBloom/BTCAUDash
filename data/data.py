49b341deccb4e5d54a39b9a7cd233ee628526a4f
Codey Bloom <codeybloom@Codeys-MacBook-Pro.local>
2025-04-26 16:05:46 +1000

    got some basic data written to parquet

--- data/data.py
+++ data/data.py
@@ -1,4 +1,5 @@
 import httpx
+import polars as pl
 
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=aud&from=1716127200&to=1745157600"
 
@@ -6,4 +7,8 @@ headers = {"accept": "application/json"}
 
 response = httpx.get(url, headers=headers)
 
-print(response.text)
+data = response.json()
+
+df = pl.DataFrame(data["prices"], schema=["timestamp", "price"])
+
+df.write_parquet("raw_data.parquet")
